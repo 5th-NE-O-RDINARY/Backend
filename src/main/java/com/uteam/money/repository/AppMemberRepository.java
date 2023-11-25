8fc7670159d680445c7070050b9b76afd96b45dd
jhsong76 <20190976@sungshin.ac.kr>
2023-11-26 02:27:42 +0900

    fix: 충돌 병합

--- src/main/java/com/uteam/money/repository/AppMemberRepository.java
+++ src/main/java/com/uteam/money/repository/AppMemberRepository.java
@@ -3,10 +3,9 @@ package com.uteam.money.repository;
 import com.uteam.money.domain.AppMember;
 import com.uteam.money.domain.Appointment;
 import com.uteam.money.domain.Member;
-<<<<<<< HEAD
+
 import java.util.List;
-=======
->>>>>>> 3eb048850fcd89a75138e0575f8f1e7e0b2eb7e9
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
