06053d21a80d2b1df9fa68742606b42507dfe7b8
jhsong76 <20190976@sungshin.ac.kr>
2023-11-25 15:41:45 +0900

    feat: Member 도메인 구축 #1

---
+++ src/main/java/com/uteam/money/domain/Member.java
@@ -0,0 +1,34 @@
+package com.uteam.money.domain;
+
+import com.uteam.money.domain.common.BaseEntity;
+import com.uteam.money.domain.enums.MemberStatus;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Member extends BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long memberIdx;
+
+    private String memberId;
+    private String password;
+    private String name;
+    @Enumerated(EnumType.STRING)
+    @Column(columnDefinition = "VARCHAR(15) DEFAULT 'ACTIVE'")
+    private MemberStatus status;
+}
